4658b65700893ef16f3241b280e8eb9c88196070
ajakaiye33 <ajakzhedgar@gmail.com>
2019-03-22 12:58:05 +0100

    vector creation

---
+++ practice_R_syntax/assignments.R
@@ -0,0 +1,16 @@
+# Use various utility functions to create vectors containing these vales
+#####################1A#####################
+v1 <- 1:10
+2**v1
+
+###############2B###########################
+
+vector_1to_26 <- 1:26
+aphabet_26 <- letters
+aphabet_26
+rev_aphabet <- rev(aphabet_26)
+rev_aphabet
+comby <- paste(rev_aphabet,vector_1to_26,sep="")
+rev_as <- rev(comby)
+rev_as
+
