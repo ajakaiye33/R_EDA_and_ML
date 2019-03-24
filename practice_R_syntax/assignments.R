d14b1de23c854a7717e8e3762afb8b435b09a149
ajakaiye33 <ajakzhedgar@gmail.com>
2019-03-24 05:59:50 +0100

    rlooping

--- practice_R_syntax/assignments.R
+++ practice_R_syntax/assignments.R
@@ -3,7 +3,7 @@
 v1 <- 1:10
 2**v1
 
-###############2B###########################
+###############1B###########################
 
 vector_1to_26 <- 1:26
 aphabet_26 <- letters
@@ -13,4 +13,22 @@ rev_aphabet
 comby <- paste(rev_aphabet,vector_1to_26,sep="")
 rev_as <- rev(comby)
 rev_as
+#######################2########################
+
+addtress_list <- c(frst="802/hirannandani/Mumbai",
+                   scd="2A/kalka-Delhi",
+                   thd="345#near adyar#Chennai",
+                   fot="10-shyaam bazzar-Kolkata")
+
+first <- strsplit(addtress_list, "/")
+first$frst[3]
+second <- strsplit(addtress_list,"#")
+second$thd[3]
+third <- strsplit(addtress_list,"-")
+third$scd[2]
+third$fot[3]
+solu <- c(first$frst[3],second$thd[3],third$scd[2],third$fot[3])
+for(i in solu){
+  print(i)
+}
 
